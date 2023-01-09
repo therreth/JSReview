3d111e7ef3b5c5417696f4036d0d77aa8a2e98ab
therreth <thenry2@aceproject.space>
2023-01-09 11:38:43 -0600

    Datatypes

---
+++ Chapter 2/sandbox.js
@@ -0,0 +1,14 @@
+alert("Hello");
+console.log("MoreText");
+
+let age = 9;
+let year = 1222;
+
+console.log(age,year);
+
+age = 1;
+console.log(age);
+
+const points = 100;
+//points = 50;
+console.log(points);
