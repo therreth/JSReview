ff359edc90326958bd9e1502f0db7ad24f594408
therreth <thenry2@aceproject.space>
2023-01-09 11:49:23 -0600

    String methods

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -12,3 +12,27 @@ console.log(age);
 const points = 100;
 //points = 50;
 console.log(points);
+
+//Strings
+console.log("Hello string");
+
+let email = "mario@poo.com";
+console.log(email);
+//String Concat
+let firstname = "Bon";
+let lastname = "bonbon";
+
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+
+//getting chars
+console.log(fullname[0]);
+
+//string lenght
+console.log(fullname.length);
+
+//String methods
+
+console.log(fullname.toUpperCase());
+let result = fullname.toLowerCase();
+console.log(result, fullname);
