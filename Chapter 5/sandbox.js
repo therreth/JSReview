516c41fb6295e66a8faa477cf8c236e33993e6a4
therreth <thenry2@aceproject.space>
2023-01-10 14:55:04 -0600

    Chapter 5 Math Functions

--- Chapter 5/sandbox.js
+++ Chapter 5/sandbox.js
@@ -51,3 +51,9 @@ const area = 7.7;
 console.log(Math.round(area));
 console.log(Math.floor(area));
 console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
